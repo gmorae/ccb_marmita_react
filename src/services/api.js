c64d85c3c605a89d99596b158064288f5c7c58bb
gmorae <gmmartins06@gmail.com>
2020-02-11 02:12:21 -0200

    UP CORS

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 
 const Api = Axios.create({
-    baseURL: "https://marmitaccb.herokuapp.com/" ,
+    baseURL: "https://cors-anywhere.herokuapp.com/https://marmitaccb.herokuapp.com/" ,
     headers: {
         "Content-Type" : "application/json",
     }
