20f8227b0bdd775a87f5c451003484b6776f9088
gmorae <gmmartins06@gmail.com>
2020-03-05 22:48:21 -0300

    up

--- src/List.js
+++ src/List.js
@@ -27,30 +27,28 @@ export default class List extends Component {
         });
     }
 
-    delete = async (event) => {
-        await api.delete(`users/${event}`);
+    delete = async event => {
         toast.success('Registro excluido com sucesso')
-        setTimeout(() => {
-            window.location.reload()
-        }, 1000);
+        let item = document.getElementById(`linha${event}`)
+        item.parentNode.removeChild(item)
+        await api.delete(`users/${event}`);
     }
 
-    update = async (event) => {
+    update = async event => {
         await api.post(`users/entregue/${event}`);
-        const getOK = await getEntregue()
-        this.setState({ listEntregue: getOK.data })
-        this.state.listEntregue.forEach(element => {
-            document.getElementById(`button${element.id_ok}`).classList.add('btn-success')
-            document.getElementById(`button${element.id_ok}`).innerHTML = 'Entregue'
-        });
+        document.getElementById(`button${event}`).classList.add('btn-success')
     };
+
+    edit = event => {
+        toast.warn('Funcionalidade a ser desenvolvida')
+    }
 
     render() {
         return (
             <div>
                 <ToastContainer />
                 <Navbar />
-                <div className="container col-md-7 mx-auto">
+                <div className="container mx-auto">
                     <h2 className="mb-3 text-center mt-5">Lista de usuário</h2>
                     <h4 className="my-5"><b>Total de encomendas:</b> {this.state.total} unidades</h4>
                     <div className="table-responsive text-nowrap">
@@ -59,6 +57,7 @@ export default class List extends Component {
                                 <tr>
                                     <th scope="col">Nome Completo</th>
                                     <th scope="col">Quantidade de marmita</th>
+                                    <th scope="col">Total</th>
                                     <th scope="col">Ação</th>
                                 </tr>
                             </thead>
@@ -66,11 +65,31 @@ export default class List extends Component {
                                 {
                                     this.state.list.map((res) => {
                                         return (
-                                            <tr key={res.idusers}>
+                                            <tr key={res.idusers} id={`linha${res.idusers}`}>
                                                 <td>{res.name_user}</td>
                                                 <td>{res.qdt_marmita}</td>
-                                                <td><button type="button" id={`button${res.idusers}`} onClick={() => this.update(res.idusers)} className="btn btn-warning btn-sm">Encomendado</button>
-                                                    <button type="button" onClick={() => this.delete(res.idusers)} className="btn btn-danger btn-sm">Excluir</button></td>
+                                                <td>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(res.total)}</td>
+                                                <td>
+                                                    <button
+                                                        type="button"
+                                                        onClick={() => this.edit(res.idusers)}
+                                                        className="btn btn-blue btn-sm">
+                                                        <i class="fas fa-edit"></i>
+                                                    </button>
+                                                    <button
+                                                        type="button"
+                                                        id={`button${res.idusers}`}
+                                                        onClick={() => this.update(res.idusers)}
+                                                        className="btn btn-warning btn-sm">
+                                                        <i class="fas fa-check"></i>
+                                                    </button>
+                                                    <button
+                                                        type="button"
+                                                        onClick={() => this.delete(res.idusers)}
+                                                        className="btn btn-danger btn-sm">
+                                                        <i class="fas fa-times"></i>
+                                                    </button>
+                                                </td>
                                             </tr>
                                         )
                                     })
--- src/ListEntrega.js
+++ src/ListEntrega.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import Navbar from './navbar'
-import { getEntrega, getEntregueMoto} from './services/get';
+import { getEntrega, getEntregueMoto } from './services/get';
 import api from './services/api';
 import { toast, ToastContainer } from 'react-toastify';
 import ReactToPrint from "react-to-print";
@@ -15,48 +15,46 @@ export default class ListEntrega extends Component {
             total: Number
         }
     }
-    
+
     componentDidMount = async () => {
         const get = await getEntrega()
         const getOK = await getEntregueMoto()
         this.setState({ list: get.data.data })
         this.setState({ listEntregue: getOK.data })
-        this.setState({ total: get.data.dados.totalMamitas})
+        this.setState({ total: get.data.dados.totalMamitas })
         this.state.listEntregue.forEach(element => {
             document.getElementById(`button${element.id_ok}`).classList.add('btn-success')
             document.getElementById(`button${element.id_ok}`).innerHTML = 'Entregue'
         });
     }
 
-    delete = async (event) => {
-        await api.delete(`users/${event}`);
+    delete = async event => {
         toast.success('Registro excluido com sucesso')
-        setTimeout(() => {
-            window.location.reload()
-        }, 1000);
+        let item = document.getElementById(`linha${event}`)
+        item.parentNode.removeChild(item)
+        await api.delete(`users/${event}`);
     }
 
 
-    update = async (event) => {
+    update = async event => {
         await api.post(`/users/entregue/motoboy/${event}`);
-        const getOK = await getEntregueMoto()
-        this.setState({ listEntregue: getOK.data })
-        this.state.listEntregue.forEach(element => {
-            document.getElementById(`button${element.id_ok}`).classList.add('btn-success')
-            document.getElementById(`button${element.id_ok}`).innerHTML = 'Entregue'
-        });
+        document.getElementById(`button${event}`).classList.add('btn-success')
     };
+
+    edit = event => {
+        toast.warn('Funcionalidade a ser desenvolvida')
+    }
 
     render() {
         return (
             <div>
                 <ToastContainer />
                 <Navbar />
-                <div className="container mx-auto">
+                <div className="container-fluid mx-auto">
                     <h2 className="mb-3 text-center mt-5">Lista de entregas</h2>
                     <div className="d-flex justify-content-between">
                         <ReactToPrint
-                            trigger={() => <button onClick={() => this.dnone('none')} type="button" className="btn btn-primary btn-sm">Imprimir</button>}
+                            trigger={() => <button type="button" className="btn btn-primary btn-sm">Imprimir</button>}
                             content={() => this.componentRef}
                         />
                         <h4 className="my-1"><b>Total de encomendas:</b> {this.state.list.reduce((total, valor) => total + valor.qdt_marmita * 1, 0)} unidades</h4>
@@ -68,19 +66,40 @@ export default class ListEntrega extends Component {
                                     <th scope="col">Nome Completo</th>
                                     <th scope="col">Quantidade de marmita</th>
                                     <th scope="col">Endereço</th>
-                                    <th id="none" scope="col">Ação</th>
+                                    <th scope="col">Total</th>
+                                    <th scope="col">Ação</th>
                                 </tr>
                             </thead>
                             <tbody >
                                 {
                                     this.state.list.map((res) => {
                                         return (
-                                            <tr>
+                                            <tr id={`linha${res.idusers}`}>
                                                 <td>{res.name_user}</td>
                                                 <td>{res.qdt_marmita}</td>
                                                 <td>{res.endereco}</td>
-                                                <td id="none"><button type="button" id={`button${res.idusers}`} onClick={() => this.update(res.idusers)} className="btn btn-warning btn-sm">Encomendado</button>
-                                                    <button type="button" onClick={() => this.delete(res.idusers)} className="btn btn-danger btn-sm">Excluir</button></td>
+                                                <td>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(res.total)}</td>
+                                                <td>
+                                                    <button
+                                                        type="button"
+                                                        onClick={() => this.update(res.idusers)}
+                                                        className="btn btn-blue btn-sm">
+                                                        <i class="fas fa-edit"></i>
+                                                    </button>
+                                                    <button
+                                                        type="button"
+                                                        id={`button${res.idusers}`}
+                                                        onClick={() => this.update(res.idusers)}
+                                                        className="btn btn-warning btn-sm">
+                                                        <i class="fas fa-check"></i>
+                                                    </button>
+                                                    <button
+                                                        type="button"
+                                                        onClick={() => this.delete(res.idusers)}
+                                                        className="btn btn-danger btn-sm">
+                                                        <i class="fas fa-times"></i>
+                                                    </button>
+                                                </td>
                                             </tr>
                                         )
                                     })
