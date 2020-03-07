02ed7ab995b97c85fa6ee0f9ba14ca8dea6e1158
Gabriel Moraes Martins <gmmartins@stefanini.com>
2020-03-07 09:37:08 -0300

    up relatorio

--- src/Relatorio.js
+++ src/Relatorio.js
@@ -1,12 +1,7 @@
 import React, { Component } from 'react'
 import Navbar from './navbar'
 import {
-    getEntregue,
-    getUsers,
-    getEntrega,
-    getRetirada,
-    getDoacao,
-    getEntregueMoto
+    relatorio, getDoacao
 } from './services/get';
 
 export default class Relatorio extends Component {
@@ -14,35 +9,23 @@ export default class Relatorio extends Component {
         super();
         this.state = {
             date: '',
-            listEntregue: [],
             list: [],
-            listRetirada: [],
-            listEntrega: [],
-            listDoacao: [],
-            listMotoboy: []
+            doacao: Number
         }
     }
 
     componentDidMount = async () => {
-        const getUser = await getUsers()
-        const getOK = await getEntregue()
-        const retirada = await getRetirada()
-        const entrega = await getEntrega()
-        const doacao = await getDoacao()
-        const motoboy = await getEntregueMoto()
+        const dados = await relatorio()
+        const dadosDoacao = await getDoacao()
 
-        this.setState({ listEntregue: getOK.data })
-        this.setState({ list: getUser.data })
-        this.setState({ listRetirada: retirada.data })
-        this.setState({ listEntrega: entrega.data })
-        this.setState({ listDoacao: doacao.data })
-        this.setState({ listMotoboy: motoboy.data })
-
-
-
+        this.setState({ list: dados.data.dados })  
+        this.setState({doacao: dadosDoacao.data.total})
+        
         var data = new Date();
         this.setState({ date: `${data.getDate()}/${(data.getMonth() + 1)}/${data.getFullYear()}` })
     }
+
+    format = value => new Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'BRL'}).format(value)
 
     render() {
         return (
@@ -53,30 +36,24 @@ export default class Relatorio extends Component {
                         <h2 className="mb-3 text-center mt-5 mb-3">Relatório do dia {this.state.date}</h2>
                         <div className="mt-5">
                             <div className="d-flex mt-5 justify-content-between">
-                                <h5><b>Total de marmitas à ser vendida</b></h5>
-                                <h5>R$ {this.state.list.reduce((total, valor) => total + valor.qdt_marmita, 0) * 15} </h5>
-                            </div>
-                            <div className="d-flex mt-5 justify-content-between">
                                 <h5><b>Total de marmitas que foi retirada </b></h5>
-                                <h5>R$ {this.state.listRetirada.reduce((total, valor) => total + valor.qdt_marmita, 0) * 15}</h5>
+                                <h5>{this.format(this.state.list.totalRetirada)}</h5>
                             </div>
                             <div className="d-flex mt-5 justify-content-between">
                                 <h5><b>Total de marmitas que foi entregue</b></h5>
-                                <h5>R$ {this.state.listEntrega.reduce((total, valor) => total + valor.qdt_marmita + 2, 0) * 15}</h5>
+                                <h5>{this.format(this.state.list.totalEntrega)}</h5>
                             </div>
                             <div className="d-flex mt-5 justify-content-between">
                                 <h5><b>Total de marmitas vendidas</b></h5>
-                                <h5>R$ {(this.state.listEntregue.length * 15) + ((this.state.listMotoboy.length * 15) + 2)}</h5>
+                                <h5>{this.format(this.state.list.totalMarmitas)}</h5>
                             </div>
                             <div className="d-flex mt-5 justify-content-between">
                                 <h5><b>Total de doação</b></h5>
-                                <h5>R$ {this.state.listDoacao.reduce((t, v) => t + v.valor * 1, 0)}</h5>
+                                <h5>{this.format(this.state.doacao)}</h5>
                             </div>
                             <div className="d-flex mt-5 justify-content-end">
                                 <h5><b>TOTAL GERAL</b></h5>
-                                <h5 className="ml-2">R$
-                                {(this.state.listEntregue.length * 15) + ((this.state.listMotoboy.length * 15) + 2) + this.state.listDoacao.reduce((t, v) => t + v.valor * 1, 0)}
-                                </h5>
+                                <h5 className="ml-2">{this.format(this.state.list.totalMarmitas + this.state.doacao)}</h5>
                             </div>
                         </div>
                     </div>
--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import Axios from 'axios';
 
 const Api = Axios.create({
-    baseURL: "https://cors-anywhere.herokuapp.com/https://marmitaccb.herokuapp.com/" ,
-    //baseURL: 'http://localhost:2222/',
+    //baseURL: "https://cors-anywhere.herokuapp.com/https://marmitaccb.herokuapp.com/" ,
+    baseURL: 'http://localhost:2222/',
     headers: {
         "Content-Type" : "application/json",
     }
